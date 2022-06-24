f6fb639b9d6e78a08ecb83fb451bafd7fb7a62a8
Podolyakin <podolyakinqq@gmail.com>
2022-06-24 17:56:05 +0500

    done

--- Weather/ViewControllers/MainViewController.swift
+++ Weather/ViewControllers/MainViewController.swift
@@ -7,4 +7,31 @@
 
 import UIKit
 
+class MainViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    private let cities = CitiesDataManager.shared.cities
+    @IBOutlet var cityPicker: UIPickerView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        cityPicker.dataSource = self
+        cityPicker.delegate = self
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent: Int) -> Int {
+        cities.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        cities[row].city
+    }
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let detailsVC = segue.destination as? DetailsViewController else { return }
+        detailsVC.city = cities[cityPicker.selectedRow(inComponent: 0)]
+    }
 
+}
